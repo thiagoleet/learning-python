464cd373212eb74b7a041d28577f7821bffc9eca
Thiago Leite <thiagofmleite@gmail.com>
2024-04-19 17:46:06 -0300

    Módulo 01: Projeto - adicionando tarefas

--- Modulo01/projeto/gerenciador.py
+++ Modulo01/projeto/gerenciador.py
@@ -1,3 +1,16 @@
+def adicionar_tarefa(tarefas, nome_tarefa="tarefa padrão"):
+    # tarefa : nome da tarefa
+    # completada: indica se a tarefa está completada
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+
+    print(f"\nTarefa '{nome_tarefa}' adicionada com sucesso!")
+    return
+
+
+tarefas = []
+
+
 while True:
     print("\nMenu do Gerenciador de Lista de Tarefas")
     opcoes = ["Adicionar tarefa", "Ver tarefas",
@@ -6,7 +19,12 @@ while True:
     for index, opcao in enumerate(opcoes):
         print(f"{index + 1}. {opcao}")
 
-    escolha = input("\nDigite a sua escolha:")
+    escolha = input("\nDigite a sua escolha: ")
 
-    if escolha == "6":
+    if escolha == "1":
+        nome_tarefa = input("\nDigite o nome da tarefa: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "6":
         break
+
+print("\nPrograma finalizado")
