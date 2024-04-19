aceea474b98585663484f8f1f2f93cf8d54d6b39
Thiago Leite <thiagofmleite@gmail.com>
2024-04-19 17:55:56 -0300

    Módulo 01: Projeto - visualizando tarefas

--- Modulo01/projeto/gerenciador.py
+++ Modulo01/projeto/gerenciador.py
@@ -8,6 +8,16 @@ def adicionar_tarefa(tarefas, nome_tarefa="tarefa padrão"):
     return
 
 
+def ver_tarefas(tarefas):
+    print("\nLista de Tarefas:")
+    for index, tarefa in enumerate(tarefas, start=1):
+        nome_tarefa = tarefa["tarefa"]
+        status = "√" if tarefa["completada"] else " "
+        print(
+            f"{index}. [{status}] {nome_tarefa}")
+    return
+
+
 tarefas = []
 
 
@@ -24,6 +34,8 @@ while True:
     if escolha == "1":
         nome_tarefa = input("\nDigite o nome da tarefa: ")
         adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "2":
+        ver_tarefas(tarefas)
     elif escolha == "6":
         break
 
