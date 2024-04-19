8226008da2566546241ed3620f2049ee7b448782
Thiago Leite <thiagofmleite@gmail.com>
2024-04-19 17:20:18 -0300

    Método WHILE

---
+++ Modulo01/while.py
@@ -0,0 +1,17 @@
+# Estruturas de laço: WHILE
+
+print("Exemplo de While")
+contador = 0
+
+while contador < 5:
+    print(f"Contagem: {contador}")
+    contador += 1
+
+# # Usando break
+# while contador < 5:
+#     print(f"Contagem: {contador}")
+#     contador += 1
+#     if contador == 4:
+#         break
+
+print("Programa finalizado")
