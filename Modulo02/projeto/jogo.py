ad59ee099c3afe9ed3b506284a45decaf230d8b4
Thiago Leite <thiagofmleite@gmail.com>
2024-04-22 17:05:44 -0300

    Módulo 02: Projeto - criando o orquestrador do jogo

--- Modulo02/projeto/jogo.py
+++ Modulo02/projeto/jogo.py
@@ -2,8 +2,29 @@
 from heroi import Heroi
 from inimigo import Inimigo
 
-heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
-print(heroi.exibir_detalhes())
 
-inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
-print(inimigo.exibir_detalhes())
+class Jogo:
+    """ Classe orquestradora do jogo """
+
+    def __init__(self) -> None:
+        self.heroi = Heroi(nome="Herói", vida=100, nivel=5,
+                           habilidade="Super Força")
+        self.inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
+
+    def iniciar_batalha(self):
+        """ Fazer a gestão da batalha em turnos """
+        print("\nIniciando a batalha")
+        while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
+            print("\nDetalhes dos Personagems:")
+            print(self.heroi.exibir_detalhes())
+            print("\n")
+            print(self.inimigo.exibir_detalhes())
+
+            input("\nPressione Enter para atacar...")
+            escolha = input(
+                "Escolha (1) Ataque Normal - (2) Ataque Especial:")
+
+
+# Criar instância do jogo e iniciar a batalha
+jogo = Jogo()
+jogo.iniciar_batalha()
