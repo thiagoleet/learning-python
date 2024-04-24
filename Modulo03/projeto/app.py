1240d86563f48875aeef321c27338016e0b57a0c
Thiago Leite <thiagofmleite@gmail.com>
2024-04-24 20:37:46 -0300

    Módulo 03: Projeto - Criando um Crud

---
+++ Modulo03/projeto/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+app = Flask(__name__)
+
+tasks = []
+task_id_control = 1
+
+# CRUD
+
+# Create
+
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control,
+                    title=data["title"],
+                    description=data.get("description", ""))
+    task_id_control += 1
+
+    print(new_task.to_dict())
+    tasks.append(new_task)
+    return jsonify({"message": "Nova tarefa criada com sucesso!", "task": new_task.to_dict()}), 201
+
+
+# Read
+
+if __name__ == "__main__":
+    app.run(debug=True)
+else:
+    app.run(debug=False)
