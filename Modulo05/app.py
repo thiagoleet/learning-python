55229e366a3aceed486c7855223258b2ada999f0
Thiago Leite <thiagofmleite@gmail.com>
2024-04-29 22:20:26 -0300

    Módulo 05: Adicionando o template HTML

--- Modulo05/app.py
+++ Modulo05/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, render_template
 from datetime import datetime, timedelta
 from repository.database import db
 from db_models.payment import Payment
@@ -50,7 +50,7 @@ def pix_confirmation():
 
 @app.route("/payments/pix/<int:payment_id>", methods=["GET"])
 def payment_pix_page(payment_id):
-    return "pagamento pix"
+    return render_template("payment.html")
 
 
 if __name__ == "__main__":
