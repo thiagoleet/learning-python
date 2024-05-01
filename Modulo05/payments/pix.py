44455c35ad83ed8af9b057633e46277bdaecd732
Thiago Leite <thiagofmleite@gmail.com>
2024-05-01 09:55:11 -0300

    Módulo 05: Criando testes unitários

--- Modulo05/payments/pix.py
+++ Modulo05/payments/pix.py
@@ -6,7 +6,7 @@ class Pix:
     def __init__(self) -> None:
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # criar pagamento na instituição financeira
         bank_payment_id = str(uuid.uuid4())
 
@@ -16,7 +16,7 @@ class Pix:
         # qr_code
         img = qrcode.make(hash_payment)
         qr_code_path = f"qr_code_payment_{bank_payment_id}"
-        img.save(f"static/img/{qr_code_path}.png")
+        img.save(f"{base_dir}static/img/{qr_code_path}.png")
 
         return {
             "bank_payment_id": bank_payment_id,
